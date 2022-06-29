21c23d5a35cb48bb4ffeb60b9bdbf4bf3f9ac91f
Mau Culasso <108375304+Mauro86@users.noreply.github.com>
2022-06-28 21:18:50 -0300

    Add files via upload

---
+++ CalculoMasaCorporal/src/main/java/com/mycompany/sistemagestion/Main.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.sistemagestion;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+    
+} 
